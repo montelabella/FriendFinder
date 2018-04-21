cdf69635b8f3a453271d6fbd7f604b1e6e2ab701
Crystal Hughes <hugachia@me.com>
2018-04-20 22:46:38 -0500

    adding html routes file

---
+++ FriendFinder/app/routingcd/htmlRoutes.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+	// HTML GET Requests
+	app.get('/survey', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    });
+    
+    // If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
