0dd42d34014971af63fa71df2e84fa539f2fa919
Crystal Hughes <hugachia@me.com>
2018-04-21 21:01:08 -0500

    friends array

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+       "name":"Eminem AKA Slim Shady",
+       "photo":"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTQ3NjM5MTEzMTc5MjEwODI2/eminem_photo_by_dave_j_hogan_getty_images_entertainment_getty_187596325.jpg",
+       "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+       "name":"Chewbacca",
+       "photo":"https://www.sideshowtoy.com/wp-content/uploads/2016/06/star-wars-the-force-awakens-chewbacca-sixth-scale-hot-toys-feature-902759.jpg",
+       "scores":[4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+       "name":"Jack AKA The Ripper",
+       "photo":"https://cdn.inquisitr.com/wp-content/uploads/2015/11/Jack-The-Ripper-Identified-Again-670x388.jpg",
+       "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+       "name":"Stormy Daniels",
+       "photo":"https://images-production.global.ssl.fastly.net/uploads/posts/image/151571/stormy-daniels.jpg?auto=compress&crop=faces,top&fit=crop&h=421&q=55&w=750",
+       "scores":[3,3,4,2,2,1,3,2,2,3]
+    },
+     {
+        "name":"Barbara Streisand",
+        "photo":"https://ia.media-imdb.com/images/M/MV5BMjMwOTQ3NTg1MF5BMl5BanBnXkFtZTcwMjc4MjQ4OA@@._V1_SY1000_CR0,0,699,1000_AL_.jpg",
+        "scores":[4,3,4,1,5,2,5,3,1,4]
+     },
+       {
+       "name":"Peter Dinklage",
+       "photo":"https://ia.media-imdb.com/images/M/MV5BMTM1MTI5Mzc0MF5BMl5BanBnXkFtZTYwNzgzOTQz._V1_UY317_CR20,0,214,317_AL_.jpg",
+       "scores":[3,3,4,1,4,3,5,3,5,4]
+    },
+     {
+        "name":"Yolandi",
+        "photo":"https://i.pinimg.com/564x/b5/39/2f/b5392f5b499e78edf181fcd19ea28144.jpg",
+        "scores":[4,4,2,3,2,2,3,2,4,5]
+     },
+     {
+         "name":"Justin Trudeau",
+         "photo":"https://img.wennermedia.com/620-width/r1293_fea_trudeau_newopener-f0044823-685f-4eb5-adbc-a5d63f06120f.jpg",
+         "scores": [5,2,1,2,2,1,3,2,4,5]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
